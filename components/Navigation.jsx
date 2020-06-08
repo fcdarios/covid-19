a918de0620540acbcb4dd9779d8ccdc80ebe0781
hansel <hansel.ramoscorp@outlook.com>
2020-06-08 18:32:33 -0500

    Agregando mas li

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,56 +1,80 @@
 import Link from "next/link";
 import { getRol } from '../src/roles'
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 const Navigation = (props) => {
   const [rol, setRol] = useState('paciente');
   const [usuario, setUsuario] = useState(props.usuario);
 
   useEffect(() => {
     setRol(JSON.parse(getRol()));
-  },[])
+  }, [])
 
-  let li; 
+  let li; let liMenu;
   if (!props.logged) {
-    li =  <><li className="nav-item">
-            <Link href="/login">
-              <a className="nav-link">
-                <span>Iniciar sesion</span>
-              </a>
-            </Link>
-          </li>
-          <li className="nav-item">
-          <Link href="/signup">
+    li = <><li className="nav-item">
+      <Link href="/login">
+        <a className="nav-link">
+          <span>Iniciar sesion</span>
+        </a>
+      </Link>
+    </li>
+      <li className="nav-item">
+        <Link href="/signup">
+          <a className="nav-link">
+            <span>Registrarse</span>
+          </a>
+        </Link>
+      </li></>;
+  } else {
+
+    li = <li className="nav-item ">
+      <div className="nav-link dropdown">
+        <button className="dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          {usuario.name}
+        </button>
+        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <Link href={'/' + rol + ''}  >
+            <a className="dropdown-item">
+              <span>Perfil</span>
+            </a>
+          </Link>
+          <Link href="/">
+            <a className="dropdown-item">
+              <span>Settings</span>
+            </a>
+          </Link>
+          <Link href="/logout">
+            <a className="dropdown-item">
+              <span>LogOut</span>
+            </a>
+          </Link>
+        </div>
+      </div>
+    </li>;
+    if (rol == 'medico') {
+      liMenu = <>
+        <li className="nav-item">
+          <Link href="/Services">
             <a className="nav-link">
-              <span>Registrarse</span>
+              <span>Services</span>
+            </a>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link href="/">
+            <a className="nav-link">
+              <span>Consultations</span>
+            </a>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link href="/Invoices">
+            <a className="nav-link">
+              <span>invoices</span>
             </a>
           </Link>
         </li></>;
-  } else {
-
-    li =  <li className="nav-item ">
-            <div className="nav-link dropdown">
-              <button className="dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                {usuario.name} 
-              </button>
-              <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <Link href={'/'+rol+''}  >
-                  <a className="dropdown-item">
-                    <span>Perfil</span>
-                  </a>
-                </Link>
-                <Link href="/">
-                  <a className="dropdown-item">
-                    <span>Settings</span>
-                  </a>
-                </Link>
-                <Link href="/logout">
-                  <a className="dropdown-item">
-                    <span>LogOut</span>
-                  </a>
-                </Link>
-              </div>
-            </div>
-          </li>;
+    }
   }
 
 
@@ -59,7 +83,7 @@ const Navigation = (props) => {
       <nav className="navbar navbar-expand-lg navbar-dark ftco_navbar ftco-navbar-light site-navbar-target" id="ftco-navbar">
         <div className="container">
           <Link href="/">
-                <a className="navbar-brand" >Covid-19<span>.</span></a>
+            <a className="navbar-brand" >Covid-19<span>.</span></a>
           </Link>
           <button className="navbar-toggler js-fh5co-nav-toggle fh5co-nav-toggle" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="oi oi-menu" /> Menu
@@ -67,19 +91,20 @@ const Navigation = (props) => {
           <div className="collapse navbar-collapse" id="ftco-nav">
             <ul className="navbar-nav nav ml-auto">
               <li className="nav-item">
-                  <Link href="/about">
-                      <a className="nav-link">
-                        <span>About</span>
-                      </a>
-                  </Link>
+                <Link href="/about">
+                  <a className="nav-link">
+                    <span>About</span>
+                  </a>
+                </Link>
               </li>
               <li className="nav-item">
-                  <Link href="/contact">
-                      <a className="nav-link">
-                        <span>Contact</span>
-                      </a>
-                  </Link>
-              </li>    
+                <Link href="/contact">
+                  <a className="nav-link">
+                    <span>Contact</span>
+                  </a>
+                </Link>
+              </li>
+              {liMenu}
               {li}
             </ul>
           </div>
