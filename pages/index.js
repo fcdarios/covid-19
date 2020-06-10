1ee2ed825ce61c561b6c5eb9eed894c8347d05c3
hansel <hansel.ramoscorp@outlook.com>
2020-06-09 20:40:19 -0500

    Agregando carrusel al inicio

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Main from '../components/Main';
 import Container from '../components/Container';
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 
 
 const Index = () => {
@@ -9,7 +9,7 @@ const Index = () => {
   const [user, setUser] = useState(null);
   const [logged, setLogged] = useState(false);
 
-  useEffect(() =>  {
+  useEffect(() => {
     if (localStorage.getItem('usuario') != null) {
       setUser(JSON.parse(localStorage.getItem('usuario')));
       setLogged(true);
@@ -21,14 +21,39 @@ const Index = () => {
   let html
   if (loading) {
     html = <div>Cargando</div>
-  }else{
-    html = 
-    <Container usuario={user} logged={logged} >
-        <h1>Index</h1>
-    </Container>
+  } else {
+    html =
+      <Container usuario={user} logged={logged} >
+        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+          <ol className="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src="3984558.jpeg" className="d-block w-100" alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src="invoice.jpeg" className="d-block w-100" alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src="services.jpg" className="d-block w-100" alt="..." />
+            </div>
+          </div>
+          <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="sr-only">Next</span>
+          </a>
+        </div>
+      </Container>
   }
 
-  return ( 
+  return (
     <Main title='Home'>
       {html}
     </Main>
