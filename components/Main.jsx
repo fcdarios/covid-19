16b47ff7c1ecb7f5d839a7310498f7271c214b4c
hansel <hansel.ramoscorp@outlook.com>
2020-06-09 21:04:26 -0500

    Agregue una linea para que pueda funcionar el carrusel

--- components/Main.jsx
+++ components/Main.jsx
@@ -5,6 +5,7 @@ const Main = (props) => {
     return (
         <div>
             <Head>
+                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
                 <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/materia/bootstrap.min.css" rel="stylesheet" integrity="sha384-uKLgCN8wZ+yo4RygxUNFhjywpL/l065dVTzvLuxys7LAIMmhZoLWb/1yP6+mF925" crossOrigin="anonymous" />
                 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
